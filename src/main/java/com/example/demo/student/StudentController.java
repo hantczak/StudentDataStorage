882ac31584c4c2e1606aaeed32e20a45ec2a15af
hantczak <hantczak8@gmail.com>
2021-01-26 19:04:38 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,28 @@
+package com.example.demo.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/student")
+
+
+public class StudentController {
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    private final StudentService studentService;
+
+    @GetMapping
+    public List<Student> getStudents() {
+        return studentService.getStudents();
+    }
+
+
+}
