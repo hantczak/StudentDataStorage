48df22c5631a319228d8314e167818a65395159b
hantczak <hantczak8@gmail.com>
2021-03-17 21:41:18 +0100

    Student class now has Lombok annotations instead of boilerplate constructors and getters

--- src/main/java/com/example/demo/grade/domain/Grade.java
+++ src/main/java/com/example/demo/grade/domain/Grade.java
@@ -1,60 +1,16 @@
 package com.example.demo.grade.domain;
 
-import java.time.LocalDate;
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@Getter
 public class Grade{
     private final int gradeId;
     private final GradeScale gradeScale;
     private final long studentId;
     private final LocalDate insertionDate;
     private final int gradeWeight;
-
-    public Grade(int gradeId, GradeScale gradeScale, long studentId, LocalDate insertionDate, int gradeWeight) {
-        this.gradeId = gradeId;
-        this.gradeScale = gradeScale;
-        this.studentId = studentId;
-        this.insertionDate = insertionDate;
-        this.gradeWeight = gradeWeight;
-    }
-
-    public GradeScale getGradeScale() {
-        return gradeScale;
-    }
-
-    public long getStudentId() {
-        return studentId;
-    }
-
-    public int getGradeValue() {
-        return this.gradeScale.getGradeValue();
-    }
-
-    public int getGradeId() {
-        return gradeId;
-    }
-
-    public LocalDate getInsertionDate() {
-        return insertionDate;
-    }
-
-    public int getGradeWeight() {
-        return gradeWeight;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Grade grade = (Grade) o;
-        return studentId == grade.studentId &&
-                gradeWeight == grade.gradeWeight &&
-                gradeScale == grade.gradeScale &&
-                insertionDate.equals(grade.insertionDate);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(studentId, insertionDate, gradeWeight);
-    }
 }
--- src/main/java/com/example/demo/student/domain/Student.java
+++ src/main/java/com/example/demo/student/domain/Student.java
@@ -2,12 +2,10 @@ package com.example.demo.student.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @AllArgsConstructor
-@NoArgsConstructor
 @Getter
 public class Student {
 
