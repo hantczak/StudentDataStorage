7a49eac2a92d5bf57aebe7c47ebf4897f35670d7
Hubert <hantczak8@gmail.com>
2021-06-09 17:40:51 +0200

    Student average sorting is fully operational. Unit tests also got modified.

---
+++ src/main/java/hantczak/studentDataStorage/average/domain/StudentAverageSortType.java
@@ -0,0 +1,8 @@
+package hantczak.studentDataStorage.average.domain;
+
+public enum StudentAverageSortType {
+    VALUE_ASC,
+    VALUE_DSC,
+    STUDENT_ID_ASC,
+    STUDENT_ID_DSC
+}
--- src/test/java/hantczak/studentDataStorage/average/domain/StudentAverageFacadeTest.java
+++ src/test/java/hantczak/studentDataStorage/average/domain/StudentAverageFacadeTest.java
@@ -11,6 +11,7 @@ import hantczak.studentDataStorage.student.domain.StudentFacadeConfiguration;
 import org.junit.jupiter.api.*;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -28,8 +29,8 @@ class StudentAverageFacadeTest {
     }
 
     @Nested
-    @DisplayName("getAllAverages tests")
-    class getAllAveragesTests {
+    @DisplayName("getAllAveragesSorted tests")
+    class getAllAveragesSortedTests {
 
         @Test
         @DisplayName("Should return empty list")
@@ -39,12 +40,12 @@ class StudentAverageFacadeTest {
             //when
 
             //then
-            assertEquals(0, studentAverageFacade.getAllAverages().size());
+            assertEquals(0, studentAverageFacade.getAllAveragesSorted("STUDENT_ID_ASC").size());
         }
 
         @Test
-        @DisplayName("Should return two averages")
-        void shouldReturnTwoAverages() {
+        @DisplayName("Should return two averages sorted by student ID")
+        void shouldReturnTwoAveragesSortedByStudentId() {
             //given
             Student student1 = new Student(1L, "a", "a@examplemail.com", LocalDate.parse("2005-01-01"), 16, Gender.FEMALE);
             studentFacade.addStudent(student1);
@@ -62,9 +63,36 @@ class StudentAverageFacadeTest {
 
             //then
             assertAll(
-                    () -> assertEquals(2, studentAverageFacade.getAllAverages().size()),
-                    () -> assertEquals(2.2, studentAverageFacade.getAllAverages().get(0).getAverage()),
-                    () -> assertEquals(6, studentAverageFacade.getAllAverages().get(1).getAverage())
+                    () -> assertEquals(2, studentAverageFacade.getAllAveragesSorted("STUDENT_ID_ASC").size()),
+                    () -> assertEquals(2.2, studentAverageFacade.getAllAveragesSorted("STUDENT_ID_ASC").get(0).getAverage()),
+                    () -> assertEquals(6, studentAverageFacade.getAllAveragesSorted("STUDENT_ID_ASC").get(1).getAverage())
+            );
+        }
+
+        @Test
+        @DisplayName("Should return two averages sorted by value descending")
+        void shouldReturnTwoAveragesSortedByValueDSC() {
+            //given
+            Student student1 = new Student(1L, "a", "a@examplemail.com", LocalDate.parse("2005-01-01"), 16, Gender.FEMALE);
+            studentFacade.addStudent(student1);
+            Student student2 = new Student(2L, "b", "b@examplemail.com", LocalDate.parse("2008-01-01"), 13, Gender.MALE);
+            studentFacade.addStudent(student2);
+
+            Grade grade1 = new Grade(1, GradeScale.GOOD, 1L, LocalDate.parse("2020-01-01"), 2);
+            gradeFacade.addGrade(grade1);
+            Grade grade2 = new Grade(2, GradeScale.EXCELLENT, 2L, LocalDate.parse("2021-01-01"), 1);
+            gradeFacade.addGrade(grade2);
+            Grade grade3 = new Grade(3, GradeScale.FAIL, 1L, LocalDate.parse("2019-01-01"), 3);
+            gradeFacade.addGrade(grade3);
+
+            //when
+
+            //then
+            List<StudentAverage> outcomeList = studentAverageFacade.getAllAveragesSorted("VALUE_DSC");
+            assertAll(
+                    () -> assertEquals(2, outcomeList.size()),
+                    () -> assertEquals(2.2, outcomeList.get(1).getAverage()),
+                    () -> assertEquals(6, outcomeList.get(0).getAverage())
             );
         }
     }
